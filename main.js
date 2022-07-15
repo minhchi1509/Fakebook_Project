427ea760b803e9ad6ac9af22ccfe19e279e91d01
minhchi1509 <minhchico300kc@gmail.com>
2022-07-15 14:20:14 +0700

    init

---
+++ main.js
@@ -0,0 +1,97 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const homePageChildrenList = document.getElementById("Homepage").children;
+for (let x of homePageChildrenList) {
+    x.onclick = function () {
+        for (let y of homePageChildrenList) {
+            y.classList.remove("before:content-['']", "before:absolute", "before:bottom-[-6px]", "before:bg-[#1B74E4]", "before:w-full", "before:h-[5px]");
+            if (!y.classList.contains("hover:bg-[#F2F2F2]")) { //Khong active
+                y.classList.add("hover:bg-[#F2F2F2]");
+                y.children[0].src = y.children[0].src?.replace("Filled", "Rounded");
+            }
+        }
+        if (x.id != "listHomepageButton" && x.id != "listHomepagePopup" && x.id != "leftMenuControlButton") {
+            x.classList.add("before:content-['']", "before:absolute", "before:bottom-[-6px]", "before:bg-[#1B74E4]", "before:w-full", "before:h-[5px]");
+            x.classList.remove("hover:bg-[#F2F2F2]");
+            x.children[0].src = x.children[0].src?.replace("Rounded", "Filled");
+        }
+    }
+}
+
+const searchingPopup = document.getElementById("searchingPopup");
+document.getElementById("searchingBar").onclick = function () {
+    searchingPopup.style.visibility = "visible";
+}
+
+document.getElementById("searchingPopup").getElementsByTagName("img")[0].onclick = function () {
+    document.getElementById("searchingBar").getElementsByTagName("input")[0].value = document.getElementById("searchingPopup").getElementsByTagName("input")[0].value;
+    searchingPopup.style.visibility = "hidden";
+}
+
+function hiddenSearchingPopup(event) {
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+    let l = searchingPopup.offsetLeft, r = searchingPopup.offsetLeft + searchingPopup.offsetWidth;
+    let t = searchingPopup.offsetTop, b = searchingPopup.offsetTop + searchingPopup.offsetHeight;
+    if (mouseX < l || mouseX > r || mouseY < t || mouseY > b) {
+        document.getElementById("searchingBar").getElementsByTagName("input")[0].value = document.getElementById("searchingPopup").getElementsByTagName("input")[0].value;
+        searchingPopup.style.visibility = "hidden";
+    }
+}
+document.addEventListener("click", hiddenSearchingPopup);
+
+const listHomepagePopup = document.getElementById("listHomepagePopup");
+document.getElementById("listHomepageButton").onclick = function () {
+    listHomepagePopup.classList.toggle("hidden");
+}
+
+document.getElementById("watchMoreTopButton").onclick = function () {
+    document.getElementById("leftMenuTopMore").classList.toggle("hidden");
+    document.getElementById("watchMoreTopButton").classList.toggle("hidden");
+}
+
+document.getElementById("hiddenLeftMenuTop").onclick = function () {
+    document.getElementById("leftMenuTopMore").classList.toggle("hidden");
+    document.getElementById("watchMoreTopButton").classList.toggle("hidden");
+}
+
+document.getElementById("watchMoreBottomButton").onclick = function () {
+    document.getElementById("leftMenuBottomMore").classList.toggle("hidden");
+    document.getElementById("watchMoreBottomButton").classList.toggle("hidden");
+}
+
+document.getElementById("hiddenLeftMenuBottom").onclick = function () {
+    document.getElementById("leftMenuBottomMore").classList.toggle("hidden");
+    document.getElementById("watchMoreBottomButton").classList.toggle("hidden");
+}
+
+document.getElementById("leftMenuControlButton").onclick = function () {
+    let leftMenu = document.getElementById("leftMenu");
+    let newFeeds = document.getElementsByClassName("newFeeds")[0];
+    leftMenu.classList.toggle("hidden");
+    if (leftMenu.classList.contains("hidden"))
+        newFeeds.style.display = "block";
+    else
+        newFeeds.style.display = "none";
+}
+
+const storyReelsTitleList = document.getElementById("storyReelsTitle").children;
+const storyReelsContentList = document.getElementById("storyReelsContent").children;
+for (let i = 0; i < storyReelsTitleList.length; i++) {
+    storyReelsTitleList[i].onclick = function () {
+        for (let y of storyReelsTitleList) {
+            y.classList.remove("before:content-['']", "before:absolute", "before:bottom-[-6px]", "before:bg-[#1B74E4]", "before:w-full", "before:h-[5px]", "text-[#1B74E4]");
+            if (!y.classList.contains("hover:bg-[#F2F2F2]")) { //Khong active
+                y.classList.add("hover:bg-[#F2F2F2]");
+            }
+        }
+        storyReelsTitleList[i].classList.add("before:content-['']", "before:absolute", "before:bottom-[-6px]", "before:bg-[#1B74E4]", "before:w-full", "before:h-[5px]", "text-[#1B74E4]");
+        storyReelsTitleList[i].classList.remove("hover:bg-[#F2F2F2]");
+        for (let x of storyReelsContentList)
+            x.style.visibility = "hidden";
+        storyReelsContentList[i].style.visibility = "visible";
+    }
+}
